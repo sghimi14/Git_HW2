0e8e445cdcb559f88c0dcf60e67950a20658f943
Swastik Ghimire <sghimi14@asu.edu>
2023-10-15 16:49:32 -0700

    This code helps find the word mouse and mice in the text

--- wordfind.py
+++ wordfind.py
@@ -3,6 +3,6 @@ text = input("Enter some text: ")
 words = text.split()
 
 for i,word in enumerate(words):
-	if word.lower() == "mouse":
+	if word.lower() == "mouse" or word.lower() == 'mice':
 		print (f"Found '{word}' at position {i}")
 

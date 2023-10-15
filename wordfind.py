685ecb4b7c69f49bb656d796706cd05ca5f87a4d
Swastik Ghimire <sghimi14@asu.edu>
2023-10-15 16:55:50 -0700

    Here it ignores the joint words

--- wordfind.py
+++ wordfind.py
@@ -5,4 +5,5 @@ words = text.split()
 for i,word in enumerate(words):
 	if word.lower() == "mouse" or word.lower() == 'mice':
 		print (f"Found '{word}' at position {i}")
-
+	elif "mouse" in word.lower():
+		print (f"Ignoring '{word}' as it is a different word" )
